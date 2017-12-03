1b1ea2060f3fd7fad81ec4f3ab59159a6e1f9f47
alexander <ag.saldivar@gmail.com>
2017-12-03 08:44:14 -0800

    setup x-ray with indeed for cannabis query

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+/************ x-ray ***************/
+const Xray = require('x-ray')
+const x = Xray()
+
+x('https://www.indeed.com/q-cannabis-jobs.html', '[data-tn-component="organicJob"]', [{
+  details: x('.turnstileLink@href', {
+		companyName: 'span.company',
+  	jobTitle: '.jobtitle',
+  	jobLocation: 'span.location',
+		jobCompensation: '[data-tn-component="jobHeader"] div:last-child',
+		jobDescription: '.summary@html',
+  	jobApplication: '[rel="canonical"]@href'
+  })
+}])
+  .paginate('.pagination a:last-child@href')
+  .limit(2)
+  .write('results.json')
+/**********************************/
+
+app.get('/', (req, res) => res.json({ message: 'scrape on'}))
+
+app.listen(7000, () => console.log('App listening on port 7000'))
