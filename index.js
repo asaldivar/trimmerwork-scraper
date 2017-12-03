4b7940c94dadf0a20f9a85ae102b837fe151f575
alexander <ag.saldivar@gmail.com>
2017-12-03 08:54:01 -0800

    clean compensation

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const app = express()
 
 /************ x-ray ***************/
 const Xray = require('x-ray')
-const x = Xray()
+const x = Xray({
+  filters: {
+    clean: function (value){
+      return value.replace(/\r?\n/g, '').trim()
+    }
+  }
+})
 
 x('https://www.indeed.com/q-cannabis-jobs.html', '[data-tn-component="organicJob"]', [{
   details: x('.turnstileLink@href', {
 		companyName: 'span.company',
   	jobTitle: '.jobtitle',
   	jobLocation: 'span.location',
-		jobCompensation: '[data-tn-component="jobHeader"] div:last-child',
+		jobCompensation: '[data-tn-component="jobHeader"] div:last-child | clean',
 		jobDescription: '.summary@html',
   	jobApplication: '[rel="canonical"]@href'
   })
