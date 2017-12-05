c33ec785a0f9a3f7af9400b6dd543ba5e0c27e8b
alexander <ag.saldivar@gmail.com>
2017-12-05 14:55:22 -0800

    add scraped site

--- index.js
+++ index.js
@@ -1,49 +1,84 @@
 const express = require('express')
 const app = express()
+const crypto = require('crypto')
 
+/************ x-ray ***************/
+// const Xray = require('x-ray')
+// const x = Xray({
+//   filters: {
+//     trim: function(value) {
+//       return value.replace(/\r?\n/g, '').trim()
+//     },
+//     findJobType: function(value) {
+// 			return value.substring(value.lastIndexOf("Job Type:")+10,value.length)
+//     },
+//     date: function(value) {
+//     	console.log('value:',value.substring(0, value.indexOf('-')))
+//     	return value.substring(0, value.indexOf('-'))
+//     }
+//   }
+// })
+
+// x('https://www.indeed.com/jobs?q=cannabis&l=Portland%2C+OR&sort=date', '[data-tn-component="organicJob"]', [{
+//   details: x('.turnstileLink@href', {
+//   	_id: 'link[rel="alternate"]@href',
+// 		companyName: 'span.company',
+//   	jobTitle: '.jobtitle',
+//   	jobType: '.summary p:contains("Job Type")| findJobType',
+//   	jobLocation: 'span.location',
+//   	date: 'meta[name="description"]@content',
+// 		jobCompensation: '[data-tn-component="jobHeader"] div:last-child | trim',
+// 		jobDescription: '.summary@html',
+//   	jobApplication: '[rel="canonical"]@href'
+//   })
+// }])
+//   .paginate('.pagination a:last-child@href')
+//   .limit(10)
+//   .write('results.json')
+/**********************************/
 /************ x-ray ***************/
 const Xray = require('x-ray')
 const x = Xray({
-  filters: {
-    trim: function(value) {
-      return value.replace(/\r?\n/g, '').trim()
-    },
-    findJobType: function(value) {
-			return value.substring(value.lastIndexOf("Job Type:")+10,value.length)
-    },
-    date: function(value) {
-    	console.log('value:',value.substring(0, value.indexOf('-')))
-    	return value.substring(0, value.indexOf('-'))
-    }
-  }
+	filters: {
+		trim: function(value) {
+			return value.replace(/\r?\n/g, '').trim()
+		}
+	}
 })
 
-x('https://www.indeed.com/jobs?q=cannabis&l=Portland%2C+OR&sort=date', '[data-tn-component="organicJob"]', [{
-  details: x('.turnstileLink@href', {
-  	_id: 'link[rel="alternate"]@href',
-		companyName: 'span.company',
-  	jobTitle: '.jobtitle',
-  	jobType: '.summary p:contains("Job Type")| findJobType',
-  	jobLocation: 'span.location',
-  	date: 'meta[name="description"]@content',
-		jobCompensation: '[data-tn-component="jobHeader"] div:last-child | trim',
-		jobDescription: '.summary@html',
-  	jobApplication: '[rel="canonical"]@href'
-  })
-}])
-  .paginate('.pagination a:last-child@href')
-  .limit(10)
-  .write('results.json')
+x('https://420jobsearch.com/browse-jobs', '.job-list li', [{
+	city: '.loc_city',
+	state: '.loc_state',
+	date: '.job-footer span:last-child',
+	jobApplication: 'a@href',
+	details: x('a@href', {
+		companyName: '.content .inverse-link',
+		jobTitle: '.addthis_inline_share_toolbox@data-title',
+		jobType: '.job-type | trim',
+		jobDescription: '.container .eleven .padding-right | trim',
+		jobCompensation: '.job-overview li:first-child div a | trim',
+	})
+}]).paginate('.next@href').limit(2)(function(err, value) {
+	var testObject = value
+
+	let newArray = []
+
+	var foo = testObject.map(obj => {
+		let newObject = {}
+		const currentDate = (new Date()).valueOf().toString()
+		const random = Math.random().toString()
+
+		obj.details.jobLocation = obj.city + ', ' + obj.state
+		obj.details._id = crypto.createHash('sha1').update(currentDate + random).digest('hex')
+		obj.details.date = !isNaN(obj.date.charAt(1)) ? Date.now() : obj.date
+		obj.details.jobApplication = obj.jobApplication
+
+		return obj.details
+	})
+	console.log(foo)
+})
 /**********************************/
 
-// var testObject = []
-
-// let newArray = []
-
-// var foo = testObject.map(obj => {
-// 	let newObject = {}
-// 	return JSON.stringify(obj.details)
-// })
 
 app.get('/', (req, res) => res.json({ message: 'scrape on'}))
 
